3657aa937102c8e8213dca38ed8a597a94487fb9
Ammar Al-Helali <ammar0ah@gmail.com>
2019-09-10 11:33:40 +0300

    Initial Commit

---
+++ Safartas Chef/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Safartas Chef
+//
+//  Created by Ammar Al-Helali on 9/10/19.
+//  Copyright © 2019 Ammar Al-Helali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
